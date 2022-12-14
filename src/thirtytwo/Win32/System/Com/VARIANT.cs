b1e50c2633c85875c597f10b31f0b86fa3d139dd
Jeremy Kuhne <jkuhne@microsoft.com>
2022-12-13 17:48:28 -0800

    More demo for Tanya

--- src/thirtytwo/Win32/System/Com/VARIANT.cs
+++ src/thirtytwo/Win32/System/Com/VARIANT.cs
@@ -5,17 +5,53 @@ using System.Runtime.CompilerServices;
 
 namespace Windows.Win32.System.Com;
 
-public unsafe partial struct VARIANT
+public unsafe partial struct VARIANT : IDisposable
 {
     public static VARIANT Empty { get; } = default;
 
-    public bool IsEmpty => vt == VARENUM.VT_EMPTY;
+    public bool IsEmpty => vt == VARENUM.VT_EMPTY && data.llVal == 0;
 
     [UnscopedRef]
     public ref VARENUM vt => ref Anonymous.Anonymous.vt;
 
     [UnscopedRef]
     public ref _Anonymous_e__Union._Anonymous_e__Struct._Anonymous_e__Union data => ref Anonymous.Anonymous.Anonymous;
+
+    public void Dispose() => Clear();
+
+    public void Clear()
+    {
+        // PropVariantClear is essentially a superset of VariantClear it calls CoTaskMemFree on the following types:
+        //
+        //     - VT_LPWSTR, VT_LPSTR, VT_CLSID (psvVal)
+        //     - VT_BSTR_BLOB (bstrblobVal.pData)
+        //     - VT_CF (pclipdata->pClipData, pclipdata)
+        //     - VT_BLOB, VT_BLOB_OBJECT (blob.pData)
+        //     - VT_STREAM, VT_STREAMED_OBJECT (pStream)
+        //     - VT_VERSIONED_STREAM (pVersionedStream->pStream, pVersionedStream)
+        //     - VT_STORAGE, VT_STORED_OBJECT (pStorage)
+        //
+        // If the VARTYPE is a VT_VECTOR, the contents are cleared as above and CoTaskMemFree is also called on
+        // cabstr.pElems.
+        //
+        // https://learn.microsoft.com/windows/win32/api/oleauto/nf-oleauto-variantclear#remarks
+        //
+        //     - VT_BSTR (SysFreeString)
+        //     - VT_DISPATCH / VT_UNKOWN (->Release(), if not VT_BYREF)
+
+        if (IsEmpty)
+        {
+            return;
+        }
+
+        fixed (void* t = &this)
+        {
+            Interop.PropVariantClear((StructuredStorage.PROPVARIANT*)t);
+        }
+
+        vt = VARENUM.VT_EMPTY;
+        data = default;
+    }
 
     public static explicit operator int(VARIANT value)
         => value.vt == VARENUM.VT_I4 || value.vt == VARENUM.VT_INT ? value.data.intVal : ThrowInvalidCast<int>();
--- src/thirtytwo_tests/Win32/System/Com/VariantTests.cs
+++ src/thirtytwo_tests/Win32/System/Com/VariantTests.cs
@@ -7,6 +7,7 @@ using System.Runtime.InteropServices;
 using Windows.Win32;
 using Windows.Win32.Foundation;
 using Windows.Win32.System.Com;
+using Windows.Win32.UI.Accessibility;
 using static Windows.Win32.System.Com.Com;
 
 namespace Tests.Windows.Win32.System.Com;
@@ -19,32 +20,95 @@ public unsafe class VariantTests
         // COM Interop can't handle converting arbitrary struct conversion to VARIANT. The one special case type from
         // System.Drawing is Color which does the OLE color conversion to int.
         Rectangle rectangle = new(1, 2, 3, 4);
-        VARIANT variant = default;
+        using VARIANT variant = default;
         nint address = (nint)(void*)&variant;
         Assert.Throws<NotSupportedException>(() => Marshal.GetNativeVariantForObject(rectangle, address));
 
-        TestVariant test = new();
+        using TestVariant test = new();
         using ComScope<IUnknown> scope = new(GetComPointer<IUnknown>(test));
         ITestVariantComInterop comInterop = (ITestVariantComInterop)Marshal.GetObjectForIUnknown(scope);
         Assert.Throws<NotSupportedException>(() => comInterop.SetVariant(rectangle));
     }
 
+    [Fact]
+    public void MarshalArrayToVariant()
+    {
+        int[] array = new[] { 1, 2, 3, 4 };
+        using VARIANT variant = default;
+        nint address = (nint)(void*)&variant;
+        Marshal.GetNativeVariantForObject(array, address);
+        variant.vt.Should().Be(VARENUM.VT_ARRAY | VARENUM.VT_I4);
+
+        using TestVariant test = new();
+        using ComScope<IUnknown> scope = new(GetComPointer<IUnknown>(test));
+        ITestVariantComInterop comInterop = (ITestVariantComInterop)Marshal.GetObjectForIUnknown(scope);
+        comInterop.SetVariant(array);
+        test.Variant.vt.Should().Be(VARENUM.VT_ARRAY | VARENUM.VT_I4);
+    }
+
+    [Fact]
+    public void TestLegacy()
+    {
+        LegacyVariantObject legacy = new();
+        IUnknown* unknown = GetComPointer<IUnknown>(legacy);
+        using ComScope<IUnknown> scope = new(unknown);
+        using ComScope<ITestVariant> testVariant = ComScope<ITestVariant>.QueryFrom(unknown);
+        legacy.Variant = new Rectangle();
+        VARIANT variant = default;
+        testVariant.Value->GetVariant(&variant);
+    }
+
+    public class LegacyVariantObject : ITestVariantComInterop
+    {
+        public object? Variant { get; set; }
+
+        public object? GetVariant()
+        {
+            return Variant;
+        }
+
+        public void SetVariant(object? variant)
+        {
+            Variant = variant;
+        }
+
+        public void SetVariantByRef(ref object? variant)
+        {
+            Variant = variant;
+        }
+    }
 
     [ComImport]
     [Guid("3BE9EE32-26FB-4E7A-B8A8-25795A7EFB53")]
     [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
     public interface ITestVariantComInterop
     {
-        [PreserveSig]
-        public HRESULT SetVariant(object? variant);
+        public object? GetVariant();
 
-        [PreserveSig]
-        public HRESULT SetVariantByRef(ref object? variant);
+        public void SetVariant(object? variant);
+
+        public void SetVariantByRef(ref object? variant);
     }
 
-    public class TestVariant : ITestVariant.Interface, IManagedWrapper<ITestVariant>
+    public class UiaProvider : IRawElementProviderSimple.Interface, IManagedWrapper<IRawElementProviderSimple>
+    {
+        public ProviderOptions ProviderOptions => throw new NotImplementedException();
+
+        public HRESULT GetPatternProvider(UIA_PATTERN_ID patternId, IUnknown** pRetVal) => throw new NotImplementedException();
+        public HRESULT GetPropertyValue(UIA_PROPERTY_ID propertyId, VARIANT* pRetVal) => throw new NotImplementedException();
+
+        public IRawElementProviderSimple* HostRawElementProvider => throw new NotImplementedException();
+    }
+
+    public class TestVariant : ITestVariant.Interface, IDisposable, IManagedWrapper<ITestVariant>
     {
         public VARIANT Variant { get; set; }
+
+        public void Dispose()
+        {
+            Variant.Clear();
+            Variant = default;
+        }
 
         public HRESULT SetVariant(VARIANT variant)
         {
@@ -62,18 +126,33 @@ public unsafe class VariantTests
             Variant = *variant;
             return HRESULT.S_OK;
         }
+
+        public HRESULT GetVariant(VARIANT* variant) => throw new NotImplementedException();
     }
 
     public unsafe struct ITestVariant : IComIID, IVTable<ITestVariant, ITestVariant.Vtbl>
     {
+        private readonly void** _vtbl;
+
+        public HRESULT GetVariant(VARIANT* variant)
+        {
+            fixed (ITestVariant* pThis = &this)
+                return ((delegate* unmanaged[Stdcall]<ITestVariant*, VARIANT*, HRESULT>)_vtbl[3])(pThis, variant);
+        }
+
         public struct Vtbl
         {
             internal delegate* unmanaged[Stdcall]<ITestVariant*, Guid*, void**, HRESULT> QueryInterface_1;
             internal delegate* unmanaged[Stdcall]<ITestVariant*, uint> AddRef_2;
             internal delegate* unmanaged[Stdcall]<ITestVariant*, uint> Release_3;
-            internal delegate* unmanaged[Stdcall]<ITestVariant*, VARIANT, HRESULT> SetVariant_4;
-            internal delegate* unmanaged[Stdcall]<ITestVariant*, VARIANT*, HRESULT> SetVariantByRef_5;
+            internal delegate* unmanaged[Stdcall]<ITestVariant*, VARIANT*, HRESULT> GetVariant_4;
+            internal delegate* unmanaged[Stdcall]<ITestVariant*, VARIANT, HRESULT> SetVariant_5;
+            internal delegate* unmanaged[Stdcall]<ITestVariant*, VARIANT*, HRESULT> SetVariantByRef_6;
         }
+
+        [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvStdcall) })]
+        private static HRESULT GetVariant(ITestVariant* @this, VARIANT* variant)
+            => UnwrapAndInvoke<ITestVariant, Interface>(@this, o => o.GetVariant(variant));
 
         [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvStdcall) })]
         private static HRESULT SetVariant(ITestVariant* @this, VARIANT variant)
@@ -88,6 +167,7 @@ public unsafe class VariantTests
         [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
         public interface Interface
         {
+            public HRESULT GetVariant(VARIANT* variant);
             public HRESULT SetVariant(VARIANT variant);
             public HRESULT SetVariantByRef(VARIANT* variant);
         }
@@ -108,8 +188,9 @@ public unsafe class VariantTests
 
         static void IVTable<ITestVariant, Vtbl>.PopulateVTable(Vtbl* vtable)
         {
-            vtable->SetVariant_4 = &SetVariant;
-            vtable->SetVariantByRef_5 = &SetVariantByRef;
+            vtable->GetVariant_4 = &GetVariant;
+            vtable->SetVariant_5 = &SetVariant;
+            vtable->SetVariantByRef_6 = &SetVariantByRef;
         }
     }
 }
