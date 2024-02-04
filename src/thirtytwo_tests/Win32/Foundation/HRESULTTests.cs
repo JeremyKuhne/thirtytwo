452ce516e2a4738a6ba039ef7d9bf5d57d8483fe
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2024-02-03 18:07:37 -0800

    Retry the other test that has the same issue.

--- src/thirtytwo_tests/Win32/Foundation/HRESULTTests.cs
+++ src/thirtytwo_tests/Win32/Foundation/HRESULTTests.cs
@@ -7,9 +7,12 @@ namespace Windows.Win32.Foundation;
 
 public class HRESULTTests
 {
-    [Fact]
+    [RetryFact(MaxRetries = 5)]
     public void HRESULT_ToStringWithDescription()
     {
+        // The Marshal.GetExceptionForHR method is not thread safe for CLR (COR_E*) HRESULTs.
+        // We don't control all threads in the process, so we have to retry a few times.
+
         // .NET exception messages aren't localized. (Only .NET Framework)
         string message = HRESULT.COR_E_OBJECTDISPOSED.ToStringWithDescription();
         message.Should().Be("HRESULT 0x80131622 [-2146232798]: Cannot access a disposed object.");
