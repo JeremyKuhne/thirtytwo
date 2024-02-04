a7ee597284cc02fb0c6da95102071aedfbcfa2d6
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2024-02-03 18:02:07 -0800

    Missing the attribute for the discoverer

--- src/thirtytwo_tests/Xunit/RetryFactAttribute.cs
+++ src/thirtytwo_tests/Xunit/RetryFactAttribute.cs
@@ -1,7 +1,10 @@
 ﻿// Taken from Xuint samples. https://github.com/xunit/samples.xunit/tree/main/RetryFactExample
 
+using Xunit.Sdk;
+
 namespace Xunit;
 
+[XunitTestCaseDiscoverer("Xunit.RetryFactDiscoverer", "thirtytwo_tests")]
 public class RetryFactAttribute : FactAttribute
 {
     public int MaxRetries { get; set; } = 3;
