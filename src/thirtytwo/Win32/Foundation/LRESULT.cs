4cc646e752935575ab8c4c3bfbd3fd141a7208ff
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2025-05-20 18:02:15 -0700

    Implement edit common message APIs (#9)
    Add common edit message support

--- src/thirtytwo/Win32/Foundation/LRESULT.cs
+++ src/thirtytwo/Win32/Foundation/LRESULT.cs
@@ -7,4 +7,12 @@ public unsafe readonly partial struct LRESULT
 {
     public static explicit operator void*(LRESULT value) => (void*)value.Value;
     public static explicit operator LRESULT(void* value) => new((nint)value);
+
+    // #define HIWORD(l)           ((WORD)((((DWORD_PTR)(l)) >> 16) & 0xffff))
+    public ushort HIWORD => (ushort)((Value >> 16) & 0xffff);
+    public short SIGNEDHIWORD => (short)HIWORD;
+
+    // #define LOWORD(l)           ((WORD)(((DWORD_PTR)(l)) & 0xffff))
+    public ushort LOWORD => (ushort)(Value & 0xffff);
+    public short SIGNEDLOWORD => (short)LOWORD;
 }
